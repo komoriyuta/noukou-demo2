6ff9bd4bc11b98951ade0b2da645d2f2ee36db63
komoriyuta <hamucha565@gmail.com>
2024-10-04 18:19:22 +0900

    demo

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
         distDir:'build',
-        output: 'standalone',
+        assetPrefix: '/noukou-demo',
 };
 
 export default nextConfig;
